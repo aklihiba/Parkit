4fe7ae7908f1acf115a9f4afd2e898ced3512b0a
akli hiba <ih_akli@esi.dz>
2022-03-29 09:29:08 +0100

    all files included

--- app/src/main/java/com/example/projet_mobile/ParkingDetail.kt
+++ app/src/main/java/com/example/projet_mobile/ParkingDetail.kt
@@ -7,5 +7,6 @@ class ParkingDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking_detail)
+
     }
 }
