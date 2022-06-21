cbd18004f570983294badc5dce0694f1ec4b0ee9
Rima Zourane <ir_zourane@esi.dz>
2022-06-21 14:39:03 +0100

    Merged the profile page to master

--- parkit/src/main/java/com/example/parkit/Connexion.kt
+++ parkit/src/main/java/com/example/parkit/Connexion.kt
@@ -36,6 +36,7 @@ class Connexion : Fragment() {
             //TODO("personnalize empty fields warning and refactor for project")
             val mail = binding.editMail.text
             val mdp = binding.editPassword.text
+            TODO("regler cette chose")
             if (mail.isNotBlank() and mdp.isNotBlank()) {
                 var user = User(0,"","", mail.toString(),"","",mdp.toString())
                 connexion(user)
