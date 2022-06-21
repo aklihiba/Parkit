1c085a67edf456ff32922b2c93bffd1ad074e546
Rima Zourane <ir_zourane@esi.dz>
2022-06-21 14:35:51 +0100

    profile front done

--- parkit/src/main/java/com/example/parkit/Profil.kt
+++ parkit/src/main/java/com/example/parkit/Profil.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.parkit.databinding.FragmentInscriptionBinding
+import com.example.parkit.databinding.FragmentProfilBinding
 
 
 class Profil : Fragment() {
 
+    private lateinit var binding: FragmentProfilBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentProfilBinding.inflate(layoutInflater)
+        val pref by lazy {
+            requireActivity().getSharedPreferences(
+                "parkitData",
+                AppCompatActivity.MODE_PRIVATE
+            )
+        }
+        TODO("get the local infos of the logged in person et replace the text in the textfields")
+        // binding.editMail.text = pref.getString("email")
+        // binding.editName.text = pref.getString("nom")
+        // binding.editNumPhone.text = pref.getString("numero")
+
+
+        val view = binding.root
+
+        return view
     }
 
 
